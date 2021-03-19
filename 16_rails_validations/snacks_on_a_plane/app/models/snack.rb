2777cf513f19ddae7b46a140dceda94728a18ddb
Dwire <gjdwyer8@gmail.com>
2021-03-19 10:09:51 -0400

    starter

--- 16_rails_validations/snacks_on_a_plane/app/models/snack.rb
+++ 16_rails_validations/snacks_on_a_plane/app/models/snack.rb
@@ -1,4 +1,7 @@
 class Snack < ApplicationRecord
   has_many :user_snacks, dependent: :destroy
   has_many :users, through: :user_snacks
+  
+  # The user must provide a snack name
+  # flavor_profile should be one of these ['sweet', 'salty', 'bitter']
 end
