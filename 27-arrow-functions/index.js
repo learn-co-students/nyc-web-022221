f5f034f76aacb771dd87b09b2f79ab6a0b2e3e1c
Michelle Rios <brendamichelle995@gmail.com>
2021-04-07 18:07:03 -0400

    after arrow functions

--- 27-arrow-functions/index.js
+++ 27-arrow-functions/index.js
@@ -1,13 +1,20 @@
-function doubleIt(num) {
-    return num * 2
+// when there's one argument, no need for parenthesis
+const doubleIt = num => num * 2
+
+
+// createPet() // cannot call arrow function before defining it
+foo() // OK
+
+
+// need parenthesis for multiple arguments. if implicitly returning an object, wrap it in parenthesis
+const createPet = (nameStr, ageNum) => ({ name: nameStr, age: ageNum })
+
+
+function foo() {
+    console.log('hi!!!')
 }
 
-function createPet(nameStr, ageNum) {
-    return {
-        name: nameStr,
-        age: ageNum
-    }
-}
+
 
 
 
@@ -19,15 +26,40 @@ function createPet(nameStr, ageNum) {
 // Callback should return the transformation
 
 
+const transformedArr = nums.map(num => num * 3)
+
+
 
 /********************** FILTER **********************/
 // Returns a subarray satisfying a condition
 // Does not mutate the original array
 // Callback should return a boolean
 
+const filteredArr = bookTitles.filter(bookTitle => bookTitle.length > 5)
 
 
 
 /********************** SORT **********************/
 // mutates the original array
 // callback should return a number
+
+
+// nums.sort((numA, numB) => numA - numB)
+
+// nums.sort(function (numA, numB) {
+//     return numB - numA
+// })
+
+// nums.sort((numA, numB) => numB - numA)
+
+
+
+
+// bookTitles.sort((bookTitleA, bookTitleB) => bookTitleA.localeCompare(bookTitleB))
+// bookTitles.sort((bookTitleA, bookTitleB) => bookTitleB.localeCompare(bookTitleA))
+
+
+// books.sort((bookA, bookB) => {
+//     return bookA.author.localeCompare(bookB.author)
+// })
+// books.sort((bookA, bookB) => bookB.author.localeCompare(bookA.author))
