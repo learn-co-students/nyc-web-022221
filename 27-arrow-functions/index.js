cc63d8d356ab0859aa97ab4e553646b3744d8271
Michelle Rios <brendamichelle995@gmail.com>
2021-04-07 16:41:50 -0400

    before arrow functions

---
+++ 27-arrow-functions/index.js
@@ -0,0 +1,33 @@
+function doubleIt(num) {
+    return num * 2
+}
+
+function createPet(nameStr, ageNum) {
+    return {
+        name: nameStr,
+        age: ageNum
+    }
+}
+
+
+
+
+/********************** MAP **********************/
+// Transforms an array
+// Returns a new array containing the transformed values
+// (Does not mutate the original array)
+// Callback should return the transformation
+
+
+
+/********************** FILTER **********************/
+// Returns a subarray satisfying a condition
+// Does not mutate the original array
+// Callback should return a boolean
+
+
+
+
+/********************** SORT **********************/
+// mutates the original array
+// callback should return a number
