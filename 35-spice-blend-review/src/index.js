cd0e6304ce7c33926601e0b9d6f8c6c0c043a1df
Michelle Rios <brendamichelle995@gmail.com>
2021-04-14 14:24:26 -0400

    finish deliverable 1

--- 35-spice-blend-review/src/index.js
+++ 35-spice-blend-review/src/index.js
@@ -1,1 +1,25 @@
 // write your code here
+
+
+fetch('http://localhost:3000/spiceblends/1')
+    .then(response => response.json())
+    .then(spiceBlendObj => {
+        console.log(spiceBlendObj)
+        const detailImg = document.querySelector('img.detail-image')
+        detailImg.src = spiceBlendObj.image
+
+        const detailH2 = document.querySelector('h2.title')
+        detailH2.textContent = spiceBlendObj.title
+
+        spiceBlendObj.ingredients.forEach(ingredientObj => {
+            const li = document.createElement('li')
+            li.textContent = ingredientObj.name
+
+            const ingredientsUl = document.querySelector('ul.ingredients-list')
+            ingredientsUl.append(li)
+        })
+    })
+
+
+
+
