83e7ac2b105eb7989cc2fc62e488e11ea49b9af2
Michelle Rios <brendamichelle995@gmail.com>
2021-04-14 15:58:27 -0400

    finish advanced deliverables minus dynamicity for post/patch

--- 35-spice-blend-review/src/index.js
+++ 35-spice-blend-review/src/index.js
@@ -1,34 +1,73 @@
 // write your code here
 const updateTitleForm = document.querySelector('form#update-form')
+const newIngredientForm = document.querySelector('form#ingredient-form')
+const spiceImagesMenu = document.querySelector('div#spice-images')
 
 
-fetch('http://localhost:3000/spiceblends/1')
-    .then(response => response.json())
-    .then(spiceBlendObj => {
-        const detailImg = document.querySelector('img.detail-image')
-        detailImg.src = spiceBlendObj.image
+/*************** HELPER FUNCTIONS ***************/
+function addIngredientToList(name) {
+    const li = document.createElement('li')
+    li.textContent = name
 
-        const detailH2 = document.querySelector('h2.title')
-        detailH2.textContent = spiceBlendObj.title
+    const ingredientsUl = document.querySelector('ul.ingredients-list')
+    ingredientsUl.append(li)
+}
 
-        spiceBlendObj.ingredients.forEach(ingredientObj => {
-            const li = document.createElement('li')
-            li.textContent = ingredientObj.name
+function getOneSpiceBlend(id) {
+    return fetch(`http://localhost:3000/spiceblends/${id}`)
+        .then(response => response.json())
+}
 
-            const ingredientsUl = document.querySelector('ul.ingredients-list')
-            ingredientsUl.append(li)
+function getFirstSpiceBlend(id) {
+    getOneSpiceBlend(id)
+        .then(spiceBlendObj => {
+            renderDetailedSpiceBlend(spiceBlendObj)
         })
+}
+
+
+function renderDetailedSpiceBlend(spiceBlendObj) {
+    const detailImg = document.querySelector('img.detail-image')
+    detailImg.src = spiceBlendObj.image
+
+    const detailH2 = document.querySelector('h2.title')
+    detailH2.textContent = spiceBlendObj.title
+
+    const ingredientsUl = document.querySelector('ul.ingredients-list')
+    ingredientsUl.innerHTML = ''
+
+    spiceBlendObj.ingredients.forEach(ingredientObj => {
+        addIngredientToList(ingredientObj.name)
     })
+}
+
+function addMenuImage(spiceBlendObj) {
+    const img = document.createElement('img')
+    img.src = spiceBlendObj.image
+    img.dataset.id = spiceBlendObj.id
+    spiceImagesMenu.append(img)
+}
 
 
+function renderInitialPageView() {
+    fetch('http://localhost:3000/spiceblends')
+        .then(response => response.json())
+        .then(spiceBlendArr => {
+
+            getFirstSpiceBlend(spiceBlendArr[0].id)
+
+            spiceBlendArr.forEach(spiceBlendObj => {
+                addMenuImage(spiceBlendObj)
+            })
+        })
+}
 
 
+/*************** EVENT LISTENERS ***************/
 updateTitleForm.addEventListener('submit', event => {
     event.preventDefault()
 
     const title = event.target.title.value
-
-    console.log(title)
 
     fetch('http://localhost:3000/spiceblends/1', {
         method: 'PATCH',
@@ -45,17 +84,46 @@ updateTitleForm.addEventListener('submit', event => {
 })
 
 
-const newIngredientForm = document.querySelector('form#ingredient-form')
 
 newIngredientForm.addEventListener('submit', event => {
     event.preventDefault()
 
-    const newIngredient = event.target.name.value
-    const li = document.createElement('li')
-    li.textContent = newIngredient
+    const name = event.target.name.value
+    addIngredientToList(name)
 
-    const ingredientsUl = document.querySelector('ul.ingredients-list')
-    ingredientsUl.append(li)
+    fetch('http://localhost:3000/ingredients', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, spiceblendId: 1 })
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error('Uh oh! Did not work!')
+        })
+        .catch(error => {
+            alert(error.message)
+        })
 
     event.target.reset()
 })
+
+
+spiceImagesMenu.addEventListener('click', event => {
+
+    if (event.target.matches('img')) {
+
+        getOneSpiceBlend(event.target.dataset.id)
+            .then(spiceBlendObj => {
+                renderDetailedSpiceBlend(spiceBlendObj)
+            })
+
+    }
+})
+
+
+/*************** APP INIT ***************/
+renderInitialPageView()
