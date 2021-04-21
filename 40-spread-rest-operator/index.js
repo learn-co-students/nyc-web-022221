d06d3513f8af19e4b84983f2a0ca10cff6c7f897
Michelle Rios <brendamichelle995@gmail.com>
2021-04-20 22:22:32 -0400

    before lecture

---
+++ 40-spread-rest-operator/index.js
@@ -0,0 +1,157 @@
+/********************** What is the spread operator and why would I want to use it? **********************/
+
+const raffy = {
+    name: "Raffy",
+    age: 9,
+    type: "terrier",
+    bestFriends: ["Michelle", "Louie", "Coco"]
+}
+
+const pet = {
+    fluffy: true,
+    mood: "cheerful",
+    health: 100,
+    age: 99
+}
+
+// Wrong way to make a copy of Raffy
+const copyOfRaffy = raffy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************** What about arrays?? **********************/
+
+const redVeggies = ["red cabbage", "red onion", "rhubarb"]
+const greenVeggies = ["spinach", "lettuce", "zucchini"]
+
+// Wrong way to make a copy of redVeggies
+const notRealCopy = redVeggies
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************** Function Example **********************/
+// Spread
+function sumThreeNums(x, y, z) {
+    console.log(x, y, z)
+    return x + y + z
+}
+
+const arr = [3, 5, 7]
+
+// const sum = sumThreeNums(3, 5, 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************** Rest Parameter **********************/
+/* 
+- Collects all remaining elements into an array
+- Allows us to take an unlimited number of arguments 
+ into the function
+- Must be the last paramter 
+*/
+function dynamicSum(num1, num2, num3) {
+    const args = new Array(num1, num2, num3)
+    console.log(num1, num2, num3)
+
+    return args.reduce((acc, currVal) => {
+        return acc + currVal
+    }, 0)
+}
+
+const sum = dynamicSum(1, 4, 5)
+// console.log(sum)
+
+
+
+
+
+
+/********************** Rest Operator With Destructuring **********************/
+const nums = [0, 1, 2, 3, 4, 5, 6, 7]
+
+const spring = {
+    temperature: 'temperate',
+    activities: ['running', 'walks', 'hiking', 'gardening'],
+    isDaylightSaving: true
+}
+
+
